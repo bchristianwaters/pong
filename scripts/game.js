b2553b271a41f48f5590324969765168d73dc472
Christian Waters <christianwaters1993@gmail.com>
2018-05-05 22:55:02 -0400

    scoring

--- scripts/game.js
+++ scripts/game.js
@@ -7,6 +7,8 @@ var ComputerY = 260;
 var p1 = new Player(260);
 var cpu = new Computer(260);
 var ball = new Ball(300, 300, p1, cpu);
+var playerScore = 0;
+var cpuScore = 0;
 
 function redraw() {
   drawPending = false;
@@ -19,7 +21,13 @@ function redraw() {
   p1.render();
   ball.move();
   ball.render();
-  if (ball.x > 700 || ball.x < -100) {
+  if (ball.x > 700) {
+    cpuScore++
+    document.getElementById("cpuScore").innerHTML = "CPU Score: "+cpuScore.toString();
+    ball = new Ball(300, 300, p1, cpu);
+  } else if (ball.x < -100) {
+    playerScore++
+    document.getElementById("playerScore").innerHTML = "Your Score: "+playerScore.toString();
     ball = new Ball(300, 300, p1, cpu);
   }
 }
